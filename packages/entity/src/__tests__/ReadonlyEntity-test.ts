32f578e9a6b47c05928646fdeb9a5d625c780520
William Schurman <wschurman@gmail.com>
2020-06-01 13:51:39 -0700

    Add ability to check update and delete policies outside of mutation

--- packages/entity/src/__tests__/ReadonlyEntity-test.ts
+++ packages/entity/src/__tests__/ReadonlyEntity-test.ts
@@ -1,30 +1,37 @@
 import { instance, mock } from 'ts-mockito';
 
 import EntityAssociationLoader from '../EntityAssociationLoader';
+import EntityLoader from '../EntityLoader';
+import { EntityQueryContext } from '../EntityQueryContext';
 import ReadonlyEntity from '../ReadonlyEntity';
 import ViewerContext from '../ViewerContext';
 import SimpleTestEntity from '../testfixtures/SimpleTestEntity';
+import { createUnitTestEntityCompanionProvider } from '../testfixtures/createUnitTestEntityCompanionProvider';
 
 describe(ReadonlyEntity, () => {
-  it('returns correct ID', () => {
-    const viewerContext = instance(mock(ViewerContext));
-    const data = {
-      id: 'what',
-    };
-    const testEntity = new SimpleTestEntity(viewerContext, data);
-    expect(testEntity.getID()).toEqual('what');
+  describe('getID', () => {
+    it('returns correct value', () => {
+      const viewerContext = instance(mock(ViewerContext));
+      const data = {
+        id: 'what',
+      };
+      const testEntity = new SimpleTestEntity(viewerContext, data);
+      expect(testEntity.getID()).toEqual('what');
+    });
   });
 
-  it('returns correct toString', () => {
-    const viewerContext = instance(mock(ViewerContext));
-    const data = {
-      id: 'what',
-    };
-    const testEntity = new SimpleTestEntity(viewerContext, data);
-    expect(testEntity.toString()).toEqual('SimpleTestEntity[what]');
+  describe('toString', () => {
+    it('returns correct value', () => {
+      const viewerContext = instance(mock(ViewerContext));
+      const data = {
+        id: 'what',
+      };
+      const testEntity = new SimpleTestEntity(viewerContext, data);
+      expect(testEntity.toString()).toEqual('SimpleTestEntity[what]');
+    });
   });
 
-  it('cannot create instance without ID', () => {
+  it('cannot be created without an ID', () => {
     const viewerContext = instance(mock(ViewerContext));
     const dataWithoutID = {};
     expect(() => {
@@ -51,12 +58,47 @@ describe(ReadonlyEntity, () => {
     expect(testEntity.getAllFields()).toEqual(data);
   });
 
-  it('returns a new association loader', () => {
-    const viewerContext = instance(mock(ViewerContext));
-    const data = {
-      id: 'what',
-    };
-    const testEntity = new SimpleTestEntity(viewerContext, data);
-    expect(testEntity.associationLoader()).toBeInstanceOf(EntityAssociationLoader);
+  describe('associationLoader', () => {
+    it('returns a new association loader', () => {
+      const companionProvider = createUnitTestEntityCompanionProvider();
+      const viewerContext = new ViewerContext(companionProvider);
+      const data = {
+        id: 'what',
+      };
+      const testEntity = new SimpleTestEntity(viewerContext, data);
+      expect(testEntity.associationLoader()).toBeInstanceOf(EntityAssociationLoader);
+    });
+  });
+
+  describe('getRegularEntityQueryContext', () => {
+    it('creates a new regular query context', () => {
+      const companionProvider = createUnitTestEntityCompanionProvider();
+      const viewerContext = new ViewerContext(companionProvider);
+      const queryContext = SimpleTestEntity.getRegularEntityQueryContext(viewerContext);
+      expect(queryContext).toBeInstanceOf(EntityQueryContext);
+      expect(queryContext.isInTransaction()).toBe(false);
+    });
+  });
+
+  describe('runInTransactionAsync', () => {
+    it('creates a new transactional query context', async () => {
+      const companionProvider = createUnitTestEntityCompanionProvider();
+      const viewerContext = new ViewerContext(companionProvider);
+      const didCreateTransaction = await SimpleTestEntity.runInTransactionAsync(
+        viewerContext,
+        async (queryContext) => {
+          return queryContext.isInTransaction();
+        }
+      );
+      expect(didCreateTransaction).toBe(true);
+    });
+  });
+
+  describe('loader', () => {
+    it('creates a new EntityLoader', async () => {
+      const companionProvider = createUnitTestEntityCompanionProvider();
+      const viewerContext = new ViewerContext(companionProvider);
+      expect(SimpleTestEntity.loader(viewerContext)).toBeInstanceOf(EntityLoader);
+    });
   });
 });
