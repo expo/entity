3334412d1b515e4ba64008bfcf09f05118b4a8d5
William Schurman <wschurman@gmail.com>
2020-07-23 14:07:38 -0700

    Address grammar comment

--- packages/entity/src/EntityMutationValidator.ts
+++ packages/entity/src/EntityMutationValidator.ts
@@ -3,7 +3,7 @@ import ReadonlyEntity from './ReadonlyEntity';
 import ViewerContext from './ViewerContext';
 
 /*
- * A validator is a way to specify entity mutation validation that run within the
+ * A validator is a way to specify entity mutation validation that runs within the
  * same transaction as the mutation itself before creating or updating an entity.
  */
 export default abstract class EntityMutationValidator<
