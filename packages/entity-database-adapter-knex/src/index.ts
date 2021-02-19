3ce961d2f289933b7393ce80f3ca5ac4c2ba23ff
Will Schurman <wschurman@gmail.com>
2021-02-19 15:01:01 -0800

    docs: upgrade typedoc (#117)

--- packages/entity-database-adapter-knex/src/index.ts
+++ packages/entity-database-adapter-knex/src/index.ts
@@ -1,3 +1,9 @@
+/* eslint-disable tsdoc/syntax */
+/**
+ * @packageDocumentation
+ * @module @expo/entity-database-adapter-knex
+ */
+
 export { default as PostgresEntityDatabaseAdapter } from './PostgresEntityDatabaseAdapter';
 export { default as PostgresEntityDatabaseAdapterProvider } from './PostgresEntityDatabaseAdapterProvider';
 export { default as PostgresEntityQueryContextProvider } from './PostgresEntityQueryContextProvider';
