9d0be6cfa764710d84962471d1abcbfb2d140405
Quinlan Jung <quin@expo.io>
2022-02-15 17:31:30 -0800

    some pr feedback

--- packages/entity-cache-adapter-local-memory/src/GenericLocalMemoryCacher.ts
+++ packages/entity-cache-adapter-local-memory/src/GenericLocalMemoryCacher.ts
@@ -8,53 +8,72 @@ type LocalMemoryCacheValue<TFields> = Readonly<TFields> | typeof DOES_NOT_EXIST_
 export type LocalMemoryCache<TFields> = LRUCache<string, LocalMemoryCacheValue<TFields>>;
 
 type LRUCacheOptionsV7<K, V> = {
-  // the number of most recently used items to keep.
-  // note that we may store fewer items than this if maxSize is hit.
+  /**
+   * the number of most recently used items to keep.
+   * note that we may store fewer items than this if maxSize is hit.
+   */
   max: number;
 
-  // if you wish to track item size, you must provide a maxSize
-  // note that we still will only keep up to max *actual items*,
-  // so size tracking may cause fewer than max items to be stored.
-  // At the extreme, a single item of maxSize size will cause everything
-  // else in the cache to be dropped when it is added.  Use with caution!
-  // Note also that size tracking can negatively impact performance,
-  // though for most cases, only minimally.
+  /**
+   * if you wish to track item size, you must provide a maxSize
+   * note that we still will only keep up to max *actual items*,
+   * so size tracking may cause fewer than max items to be stored.
+   * At the extreme, a single item of maxSize size will cause everything
+   * else in the cache to be dropped when it is added.  Use with caution!
+   * Note also that size tracking can negatively impact performance,
+   * though for most cases, only minimally.
+   */
+
   maxSize?: number;
 
-  // function to calculate size of items.  useful if storing strings or
-  // buffers or other items where memory size depends on the object itself.
-  // also note that oversized items do NOT immediately get dropped from
-  // the cache, though they will cause faster turnover in the storage.
+  /**
+   * function to calculate size of items.  useful if storing strings or
+   * buffers or other items where memory size depends on the object itself.
+   * also note that oversized items do NOT immediately get dropped from
+   * the cache, though they will cause faster turnover in the storage.
+   */
   sizeCalculation?: (value: V, key: K) => number;
 
-  // function to call when the item is removed from the cache
-  // Note that using this can negatively impact performance.
+  /**
+   * function to call when the item is removed from the cache
+   * Note that using this can negatively impact performance.
+   */
   dispose?: (value: V, key: K) => void;
 
-  // max time to live for items before they are considered stale
-  // note that stale items are NOT preemptively removed by default,
-  // and MAY live in the cache, contributing to its LRU max, long after
-  // they have expired.
-  // Also, as this cache is optimized for LRU/MRU operations, some of
-  // the staleness/TTL checks will reduce performance, as they will incur
-  // overhead by deleting items.
-  // Must be a positive integer in ms, defaults to 0, which means "no TTL"
+  /**
+   * max time to live for items before they are considered stale
+   * note that stale items are NOT preemptively removed by default,
+   * and MAY live in the cache, contributing to its LRU max, long after
+   * they have expired.
+   * Also, as this cache is optimized for LRU/MRU operations, some of
+   * the staleness/TTL checks will reduce performance, as they will incur
+   * overhead by deleting items.
+   * Must be a positive integer in ms, defaults to 0, which means "no TTL"
+   */
   ttl?: number;
 
-  // return stale items from cache.get() before disposing of them
-  // boolean, default false
+  /**
+   * return stale items from cache.get() before disposing of them
+   * boolean, default false
+   */
   allowStale?: boolean;
 
-  // update the age of items on cache.get(), renewing their TTL
-  // boolean, default false
+  /**
+   * update the age of items on cache.get(), renewing their TTL
+   * boolean, default false
+   */
   updateAgeOnGet?: boolean;
 
-  // update the age of items on cache.has(), renewing their TTL
-  // boolean, default false
+  /**
+   * update the age of items on cache.has(), renewing their TTL
+   * boolean, default false
+   */
   updateAgeOnHas?: boolean;
 
-  // update the "recently-used"-ness of items on cache.has()
-  // boolean, default false
+  /**
+   * update the "recently-used"-ness of items on cache.has()
+   * boolean, default false
+   */
   updateRecencyOnHas?: boolean;
 };
 
@@ -64,11 +83,11 @@ export default class GenericLocalMemoryCacher<TFields> {
   static createLRUCache<TFields>(
     options: { maxSize?: number; ttlSeconds?: number } = {}
   ): LocalMemoryCache<TFields> {
-    const LRU_CACHE_MAX_AGE_SECONDS = 10;
-    const ENTITIES_LRU_CACHE_SIZE = 10000;
-    const maxAgeSeconds = options.ttlSeconds ?? LRU_CACHE_MAX_AGE_SECONDS;
+    const DEFAULT_LRU_CACHE_MAX_AGE_SECONDS = 10;
+    const DEFAULT_LRU_CACHE_SIZE = 10000;
+    const maxAgeSeconds = options.ttlSeconds ?? DEFAULT_LRU_CACHE_MAX_AGE_SECONDS;
     const lruCacheOptions: LRUCacheOptionsV7<string, TFields> = {
-      max: options.maxSize ?? ENTITIES_LRU_CACHE_SIZE,
+      max: options.maxSize ?? DEFAULT_LRU_CACHE_SIZE,
       sizeCalculation: (value: LocalMemoryCacheValue<TFields>) =>
         value === DOES_NOT_EXIST_LOCAL_MEMORY_CACHE ? 0 : 1,
       ttl: maxAgeSeconds * 1000, // convert to ms
