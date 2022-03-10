b61341057b1de2209079a2563b4219dee1a56d9e
Quinlan Jung <quin@expo.io>
2022-03-09 17:53:23 -0800

    [cache-adapter-local-memory] export classes

---
+++ packages/entity-cache-adapter-local-memory/src/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable tsdoc/syntax */
+/**
+ * @packageDocumentation
+ * @module @expo/entity-cache-adapter-local-memory
+ */
+
+export { default as LocalMemoryCacheAdapter } from './LocalMemoryCacheAdapter';
+export { default as LocalMemoryCacheAdapterProvider } from './LocalMemoryCacheAdapterProvider';
+export { default as GenericLocalMemoryCacher } from './GenericLocalMemoryCacher';
+export * from './GenericLocalMemoryCacher';
