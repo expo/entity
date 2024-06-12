7d42f863109b6fc769b63a50f484f0b80151734a
Will Schurman <wschurman@gmail.com>
2024-06-12 09:40:00 -0700

    Update barrels

--- packages/entity-database-adapter-knex/src/index.ts
+++ packages/entity-database-adapter-knex/src/index.ts
@@ -4,7 +4,8 @@
  * @module @expo/entity-database-adapter-knex
  */
 
+export * from './EntityFields';
 export { default as PostgresEntityDatabaseAdapter } from './PostgresEntityDatabaseAdapter';
 export { default as PostgresEntityDatabaseAdapterProvider } from './PostgresEntityDatabaseAdapterProvider';
 export { default as PostgresEntityQueryContextProvider } from './PostgresEntityQueryContextProvider';
-export * from './EntityFields';
+export { default as wrapNativePostgresCallAsync } from './errors/wrapNativePostgresCallAsync';
