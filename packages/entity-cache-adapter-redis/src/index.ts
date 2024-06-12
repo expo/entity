8c5f81bcca107e9b7bbea5f0ab41b0105057806e
Will Schurman <wschurman@expo.io>
2024-06-12 09:54:39 -0700

    fix: update barrels (#241)

--- packages/entity-cache-adapter-redis/src/index.ts
+++ packages/entity-cache-adapter-redis/src/index.ts
@@ -4,7 +4,8 @@
  * @module @expo/entity-cache-adapter-redis
  */
 
-export { default as RedisCacheAdapterProvider } from './RedisCacheAdapterProvider';
-export * from './RedisCommon';
 export { default as GenericRedisCacher } from './GenericRedisCacher';
 export * from './GenericRedisCacher';
+export { default as RedisCacheAdapterProvider } from './RedisCacheAdapterProvider';
+export * from './RedisCommon';
+export { default as wrapNativeRedisCallAsync } from './errors/wrapNativeRedisCallAsync';
