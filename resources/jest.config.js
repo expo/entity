93d84807da1cea9e9b553043c13bbc21d2d09f35
Nick Novitski <github@nicknovitski.com>
2025-06-20 13:33:42 -0700

    chore: Use 'v8' coverage provider

--- resources/jest.config.js
+++ resources/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  collectCoverageFrom: ['src/**/*', 'src/*', '!src/**/__*test*__/**'],
+  collectCoverageFrom: ['src/**/*', '!src/index.ts', '!src/**/__*test*__/**'],
+  coverageProvider: 'v8',
   testMatch: ['**/__tests__/**/*-test.ts'],
   transform: {
     '\\.[jt]sx?$': ['babel-jest', { rootMode: 'upward' }],
