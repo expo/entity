4c3fd175a91cf3107b1ac413e39e52e051a9dc4b
Will Schurman 🦦 <wschurman@expo.io>
2025-10-06 16:18:35 -0700

    feat: add more documentation to entity errors (#312)

--- packages/entity/src/errors/EntityCacheAdapterError.ts
+++ packages/entity/src/errors/EntityCacheAdapterError.ts
@@ -1,7 +1,14 @@
 import { EntityError, EntityErrorCode, EntityErrorState } from './EntityError';
 
+/**
+ * Base class for errors thrown by the entity cache adapter.
+ */
 export abstract class EntityCacheAdapterError extends EntityError {}
 
+/**
+ * Error thrown when a transient error occurs in the entity cache adapter.
+ * Transient errors may succeed if retried.
+ */
 export class EntityCacheAdapterTransientError extends EntityCacheAdapterError {
   public readonly state = EntityErrorState.TRANSIENT;
   public readonly code = EntityErrorCode.ERR_ENTITY_CACHE_ADAPTER_TRANSIENT;
