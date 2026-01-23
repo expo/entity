cec9581d186c7c8f10b87f657f57c4cbf13b12f0
Will Schurman <wschurman@gmail.com>
2026-01-22 20:41:16 -0700

    feat: add AllowIf[Any|All]SubRulesAllowPrivacyPolicyRule rules

---
+++ packages/entity/src/rules/AllowIfAnySubRuleAllowsPrivacyPolicyRule.ts
@@ -0,0 +1,47 @@
+import { EntityPrivacyPolicyEvaluationContext } from '../EntityPrivacyPolicy';
+import { EntityQueryContext } from '../EntityQueryContext';
+import { ReadonlyEntity } from '../ReadonlyEntity';
+import { ViewerContext } from '../ViewerContext';
+import { PrivacyPolicyRule, RuleEvaluationResult } from './PrivacyPolicyRule';
+
+export class AllowIfAnySubRuleAllowsPrivacyPolicyRule<
+  TFields extends object,
+  TIDField extends keyof NonNullable<Pick<TFields, TSelectedFields>>,
+  TViewerContext extends ViewerContext,
+  TEntity extends ReadonlyEntity<TFields, TIDField, TViewerContext, TSelectedFields>,
+  TSelectedFields extends keyof TFields = keyof TFields,
+> extends PrivacyPolicyRule<TFields, TIDField, TViewerContext, TEntity, TSelectedFields> {
+  constructor(
+    private readonly subRules: PrivacyPolicyRule<
+      TFields,
+      TIDField,
+      TViewerContext,
+      TEntity,
+      TSelectedFields
+    >[],
+  ) {
+    super();
+  }
+
+  async evaluateAsync(
+    viewerContext: TViewerContext,
+    queryContext: EntityQueryContext,
+    evaluationContext: EntityPrivacyPolicyEvaluationContext<
+      TFields,
+      TIDField,
+      TViewerContext,
+      TEntity,
+      TSelectedFields
+    >,
+    entity: TEntity,
+  ): Promise<RuleEvaluationResult> {
+    const results = await Promise.all(
+      this.subRules.map((subRule) =>
+        subRule.evaluateAsync(viewerContext, queryContext, evaluationContext, entity),
+      ),
+    );
+    return results.includes(RuleEvaluationResult.ALLOW)
+      ? RuleEvaluationResult.ALLOW
+      : RuleEvaluationResult.SKIP;
+  }
+}
---
+++ packages/entity/src/rules/__tests__/AllowIfAnySubRuleAllowsPrivacyPolicyRule-test.ts
@@ -0,0 +1,64 @@
+import { anything, instance, mock } from 'ts-mockito';
+
+import { EntityPrivacyPolicyEvaluationContext } from '../../EntityPrivacyPolicy';
+import { EntityQueryContext } from '../../EntityQueryContext';
+import { ViewerContext } from '../../ViewerContext';
+import { describePrivacyPolicyRule } from '../../utils/__testfixtures__/PrivacyPolicyRuleTestUtils';
+import { AllowIfAnySubRuleAllowsPrivacyPolicyRule } from '../AllowIfAnySubRuleAllowsPrivacyPolicyRule';
+import { AlwaysAllowPrivacyPolicyRule } from '../AlwaysAllowPrivacyPolicyRule';
+import { AlwaysDenyPrivacyPolicyRule } from '../AlwaysDenyPrivacyPolicyRule';
+import { AlwaysSkipPrivacyPolicyRule } from '../AlwaysSkipPrivacyPolicyRule';
+
+describePrivacyPolicyRule(
+  new AllowIfAnySubRuleAllowsPrivacyPolicyRule([
+    new AlwaysAllowPrivacyPolicyRule(),
+    new AlwaysSkipPrivacyPolicyRule(),
+  ]),
+  {
+    allowCases: [
+      {
+        viewerContext: instance(mock(ViewerContext)),
+        queryContext: instance(mock(EntityQueryContext)),
+        evaluationContext:
+          instance(mock<EntityPrivacyPolicyEvaluationContext<any, any, any, any, any>>()),
+        entity: anything(),
+      },
+    ],
+  },
+);
+
+describePrivacyPolicyRule(
+  new AllowIfAnySubRuleAllowsPrivacyPolicyRule([
+    new AlwaysAllowPrivacyPolicyRule(),
+    new AlwaysDenyPrivacyPolicyRule(),
+  ]),
+  {
+    allowCases: [
+      {
+        viewerContext: instance(mock(ViewerContext)),
+        queryContext: instance(mock(EntityQueryContext)),
+        evaluationContext:
+          instance(mock<EntityPrivacyPolicyEvaluationContext<any, any, any, any, any>>()),
+        entity: anything(),
+      },
+    ],
+  },
+);
+
+describePrivacyPolicyRule(
+  new AllowIfAnySubRuleAllowsPrivacyPolicyRule([
+    new AlwaysSkipPrivacyPolicyRule(),
+    new AlwaysSkipPrivacyPolicyRule(),
+  ]),
+  {
+    skipCases: [
+      {
+        viewerContext: instance(mock(ViewerContext)),
+        queryContext: instance(mock(EntityQueryContext)),
+        evaluationContext:
+          instance(mock<EntityPrivacyPolicyEvaluationContext<any, any, any, any, any>>()),
+        entity: anything(),
+      },
+    ],
+  },
+);
