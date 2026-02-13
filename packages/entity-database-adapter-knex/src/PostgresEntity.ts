701c768d1a29d269c2fede33e315a5bf4b8cbede
Will Schurman <wschurman@gmail.com>
2026-02-13 15:41:49 -0700

    feat: add PostgresEntity/ReadonlyPostgresEntity classes that expose knexLoader methods

---
+++ packages/entity-database-adapter-knex/src/PostgresEntity.ts
@@ -0,0 +1,116 @@
+import {
+  Entity,
+  EntityPrivacyPolicy,
+  EntityQueryContext,
+  IEntityClass,
+  ReadonlyEntity,
+  ViewerContext,
+} from '@expo/entity';
+
+import { AuthorizationResultBasedKnexEntityLoader } from './AuthorizationResultBasedKnexEntityLoader';
+import { EnforcingKnexEntityLoader } from './EnforcingKnexEntityLoader';
+import {
+  knexLoader as knexLoaderFn,
+  knexLoaderWithAuthorizationResults as knexLoaderWithAuthorizationResultsFn,
+} from './knexLoader';
+
+/**
+ * Abstract base class for mutable entities backed by Postgres.
+ * Provides `knexLoader` and `knexLoaderWithAuthorizationResults` as inherited static methods,
+ * in addition to the mutation methods inherited from `Entity`.
+ */
+export abstract class PostgresEntity<
+  TFields extends Record<string, any>,
+  TIDField extends keyof NonNullable<Pick<TFields, TSelectedFields>>,
+  TViewerContext extends ViewerContext,
+  TSelectedFields extends keyof TFields = keyof TFields,
+> extends Entity<TFields, TIDField, TViewerContext, TSelectedFields> {
+  /**
+   * Vend knex loader for loading entities via knex-specific methods in a given query context.
+   *
+   * @param viewerContext - viewer context of loading user
+   * @param queryContext - query context in which to perform the load
+   */
+  static knexLoader<
+    TMFields extends object,
+    TMIDField extends keyof NonNullable<Pick<TMFields, TMSelectedFields>>,
+    TMViewerContext extends ViewerContext,
+    TMEntity extends ReadonlyEntity<TMFields, TMIDField, TMViewerContext, TMSelectedFields>,
+    TMPrivacyPolicy extends EntityPrivacyPolicy<
+      TMFields,
+      TMIDField,
+      TMViewerContext,
+      TMEntity,
+      TMSelectedFields
+    >,
+    TMSelectedFields extends keyof TMFields = keyof TMFields,
+  >(
+    this: IEntityClass<
+      TMFields,
+      TMIDField,
+      TMViewerContext,
+      TMEntity,
+      TMPrivacyPolicy,
+      TMSelectedFields
+    >,
+    viewerContext: TMViewerContext,
+    queryContext: EntityQueryContext = viewerContext
+      .getViewerScopedEntityCompanionForClass(this)
+      .getQueryContextProvider()
+      .getQueryContext(),
+  ): EnforcingKnexEntityLoader<
+    TMFields,
+    TMIDField,
+    TMViewerContext,
+    TMEntity,
+    TMPrivacyPolicy,
+    TMSelectedFields
+  > {
+    return knexLoaderFn(this, viewerContext, queryContext);
+  }
+
+  /**
+   * Vend knex loader for loading entities via knex-specific methods in a given query context.
+   * Returns authorization results instead of throwing on authorization errors.
+   *
+   * @param viewerContext - viewer context of loading user
+   * @param queryContext - query context in which to perform the load
+   */
+  static knexLoaderWithAuthorizationResults<
+    TMFields extends object,
+    TMIDField extends keyof NonNullable<Pick<TMFields, TMSelectedFields>>,
+    TMViewerContext extends ViewerContext,
+    TMEntity extends ReadonlyEntity<TMFields, TMIDField, TMViewerContext, TMSelectedFields>,
+    TMPrivacyPolicy extends EntityPrivacyPolicy<
+      TMFields,
+      TMIDField,
+      TMViewerContext,
+      TMEntity,
+      TMSelectedFields
+    >,
+    TMSelectedFields extends keyof TMFields = keyof TMFields,
+  >(
+    this: IEntityClass<
+      TMFields,
+      TMIDField,
+      TMViewerContext,
+      TMEntity,
+      TMPrivacyPolicy,
+      TMSelectedFields
+    >,
+    viewerContext: TMViewerContext,
+    queryContext: EntityQueryContext = viewerContext
+      .getViewerScopedEntityCompanionForClass(this)
+      .getQueryContextProvider()
+      .getQueryContext(),
+  ): AuthorizationResultBasedKnexEntityLoader<
+    TMFields,
+    TMIDField,
+    TMViewerContext,
+    TMEntity,
+    TMPrivacyPolicy,
+    TMSelectedFields
+  > {
+    return knexLoaderWithAuthorizationResultsFn(this, viewerContext, queryContext);
+  }
+}
