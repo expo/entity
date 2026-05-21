22d7298c84cd318e23a4619746570f4ce17f6450
Will Schurman 🦦 <wschurman@expo.io>
2026-05-21 09:58:28 -0700

    fix: prevent collision in companion when entities share same name or same table name (#600)
    # Why
    
    There's nothing preventing two entity classes from sharing the same class name. While not common in practice, this preemptively defends against the issue of collisions in companion maps.
    
    Secondarily, table data coordinators could collide, even if they have different flavors of database and cache adapters. Since those are used to construct the coordinator, include their flavors in the map key.
    
    # How
    
    Update key generation code.
    
    # Test Plan
    
    Run new tests.

--- packages/entity/src/__tests__/EntityCompanionProvider-test.ts
+++ packages/entity/src/__tests__/EntityCompanionProvider-test.ts
@@ -7,6 +7,10 @@ import { EntityConfiguration } from '../EntityConfiguration.ts';
 import { StringField } from '../EntityFields.ts';
 import { EntityPrivacyPolicy } from '../EntityPrivacyPolicy.ts';
 import type { ViewerContext } from '../ViewerContext.ts';
+import { NoOpEntityMetricsAdapter } from '../metrics/NoOpEntityMetricsAdapter.ts';
+import { InMemoryFullCacheStubCacheAdapterProvider } from '../utils/__testfixtures__/StubCacheAdapter.ts';
+import { StubDatabaseAdapterProvider } from '../utils/__testfixtures__/StubDatabaseAdapterProvider.ts';
+import { StubQueryContextProvider } from '../utils/__testfixtures__/StubQueryContextProvider.ts';
 import { createUnitTestEntityCompanionProvider } from '../utils/__testfixtures__/createUnitTestEntityCompanionProvider.ts';
 
 type BlahFields = {
@@ -23,6 +27,19 @@ const blahConfiguration = new EntityConfiguration<BlahFields, 'hello'>({
     }),
   },
   databaseAdapterFlavor: 'postgres',
+  cacheAdapterFlavor: 'redis',
+});
+
+const blahOtherDatabaseConfiguration = new EntityConfiguration<BlahFields, 'hello'>({
+  idField: 'hello',
+  tableName: 'wat',
+  schema: {
+    hello: new StringField({
+      columnName: 'hello',
+      cache: false,
+    }),
+  },
+  databaseAdapterFlavor: 'other-postgres',
   cacheAdapterFlavor: 'redis',
 });
 
@@ -64,11 +81,88 @@ class NoOpTest1PrivacyPolicy extends EntityPrivacyPolicy<
   ViewerContext,
   Blah1Entity
 > {}
+
 class NoOpTest2PrivacyPolicy extends EntityPrivacyPolicy<
   BlahFields,
   'hello',
   ViewerContext,
   Blah2Entity
+> {}
+
+class BlahOtherDatabaseEntity extends Entity<BlahFields, 'hello', ViewerContext> {
+  static defineCompanionDefinition(): EntityCompanionDefinition<
+    BlahFields,
+    'hello',
+    ViewerContext,
+    BlahOtherDatabaseEntity,
+    NoOpOtherDatabasePrivacyPolicy
+  > {
+    return {
+      entityClass: BlahOtherDatabaseEntity,
+      entityConfiguration: blahOtherDatabaseConfiguration,
+      privacyPolicyClass: NoOpOtherDatabasePrivacyPolicy,
+    };
+  }
+}
+
+class NoOpOtherDatabasePrivacyPolicy extends EntityPrivacyPolicy<
+  BlahFields,
+  'hello',
+  ViewerContext,
+  BlahOtherDatabaseEntity
+> {}
+
+const DuplicateNameEntityOne = class DuplicateNameEntity extends Entity<
+  BlahFields,
+  'hello',
+  ViewerContext
+> {
+  static defineCompanionDefinition(): EntityCompanionDefinition<
+    BlahFields,
+    'hello',
+    ViewerContext,
+    InstanceType<typeof DuplicateNameEntityOne>,
+    DuplicateNamePrivacyPolicyOne
+  > {
+    return {
+      entityClass: DuplicateNameEntityOne,
+      entityConfiguration: blahConfiguration,
+      privacyPolicyClass: DuplicateNamePrivacyPolicyOne,
+    };
+  }
+};
+
+const DuplicateNameEntityTwo = class DuplicateNameEntity extends Entity<
+  BlahFields,
+  'hello',
+  ViewerContext
+> {
+  static defineCompanionDefinition(): EntityCompanionDefinition<
+    BlahFields,
+    'hello',
+    ViewerContext,
+    InstanceType<typeof DuplicateNameEntityTwo>,
+    DuplicateNamePrivacyPolicyTwo
+  > {
+    return {
+      entityClass: DuplicateNameEntityTwo,
+      entityConfiguration: blahConfiguration,
+      privacyPolicyClass: DuplicateNamePrivacyPolicyTwo,
+    };
+  }
+};
+
+class DuplicateNamePrivacyPolicyOne extends EntityPrivacyPolicy<
+  BlahFields,
+  'hello',
+  ViewerContext,
+  InstanceType<typeof DuplicateNameEntityOne>
+> {}
+class DuplicateNamePrivacyPolicyTwo extends EntityPrivacyPolicy<
+  BlahFields,
+  'hello',
+  ViewerContext,
+  InstanceType<typeof DuplicateNameEntityTwo>
 > {}
 
 describe(EntityCompanionProvider, () => {
@@ -78,5 +172,52 @@ describe(EntityCompanionProvider, () => {
     const companion2 = entityCompanionProvider.getCompanionForEntity(Blah2Entity);
     expect(companion1).not.toEqual(companion2);
     expect(companion1['tableDataCoordinator']).toEqual(companion2['tableDataCoordinator']);
+  });
+
+  it('caches companions by class identity instead of class name', () => {
+    expect(DuplicateNameEntityOne.name).toEqual(DuplicateNameEntityTwo.name);
+
+    const entityCompanionProvider = createUnitTestEntityCompanionProvider();
+    const companion1 = entityCompanionProvider.getCompanionForEntity(DuplicateNameEntityOne);
+    const companion2 = entityCompanionProvider.getCompanionForEntity(DuplicateNameEntityTwo);
+
+    expect(companion1).not.toEqual(companion2);
+    expect(companion1.entityCompanionDefinition.entityClass).toBe(DuplicateNameEntityOne);
+    expect(companion2.entityCompanionDefinition.entityClass).toBe(DuplicateNameEntityTwo);
+  });
+
+  it('does not share table data coordinators across database adapter flavors', () => {
+    const entityCompanionProvider = new EntityCompanionProvider(
+      new NoOpEntityMetricsAdapter(),
+      new Map([
+        [
+          'postgres',
+          {
+            adapterProvider: new StubDatabaseAdapterProvider(),
+            queryContextProvider: new StubQueryContextProvider(),
+          },
+        ],
+        [
+          'other-postgres',
+          {
+            adapterProvider: new StubDatabaseAdapterProvider(),
+            queryContextProvider: new StubQueryContextProvider(),
+          },
+        ],
+      ]),
+      new Map([
+        [
+          'redis',
+          {
+            cacheAdapterProvider: new InMemoryFullCacheStubCacheAdapterProvider(),
+          },
+        ],
+      ]),
+    );
+
+    const companion1 = entityCompanionProvider.getCompanionForEntity(Blah1Entity);
+    const companion2 = entityCompanionProvider.getCompanionForEntity(BlahOtherDatabaseEntity);
+
+    expect(companion1['tableDataCoordinator']).not.toEqual(companion2['tableDataCoordinator']);
   });
 });
